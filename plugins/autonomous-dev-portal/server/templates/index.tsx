8bd3fb4c754d92b83e4faf7c567d509cd04cb18b
pwatson <pwatsonr@gmail.com>
2026-07-08 11:39:07 -0500

    Revert "feat(portal): add read-only Homelab page at /portal/homelab (#651)"
    This reverts commit 00a64d8d55c59d2cd8ee8c9a15fdca6e7ff7fb3e.

--- plugins/autonomous-dev-portal/server/templates/index.tsx
+++ plugins/autonomous-dev-portal/server/templates/index.tsx
@@ -33,7 +33,6 @@ import { ApprovalsView } from "./views/approvals";
 import { AuditView } from "./views/audit";
 import { CostsView } from "./views/costs";
 import { DashboardView } from "./views/dashboard";
-import { HomelabView } from "./views/homelab";
 import { LogsView } from "./views/logs";
 import { NotFoundView } from "./views/404";
 import { OnboardView } from "./views/onboard";
@@ -100,8 +99,6 @@ function renderViewBody<V extends ViewName>(
             return <OnboardIngestionView {...(props as RenderProps["onboard-ingestion"])} />;
         case "onboard-questions":
             return <OnboardQuestionsView {...(props as RenderProps["onboard-questions"])} />;
-        case "homelab":
-            return <HomelabView {...(props as RenderProps["homelab"])} />;
         case "404":
             return <NotFoundView {...(props as RenderProps["404"])} />;
         case "500":
@@ -159,8 +156,6 @@ function activePathFor(view: ViewName): string {
             return "/onboard/ingestion";
         case "onboard-questions":
             return "/onboard/questions";
-        case "homelab":
-            return "/portal/homelab";
         case "dashboard":
         case "request-detail":
         case "404":
